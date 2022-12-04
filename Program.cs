14cf3ec19f453a8629b793f71b8af5eb199a23d1
Bill <bill.llach1801@gmail.com>
2022-12-03 23:55:14 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hola, introduzca su nombre, por favor");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hola, " + name + ". Ahora escribe la hora actual, por favor.");
+
+string time = Console.ReadLine();
+
+Console.WriteLine("La hora es " + time);
